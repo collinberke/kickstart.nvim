9c16517b9c87896dbddff1b5a236ee4e63cfc2ba
Collin K. Berke, Ph.D <32435546+collinberke@users.noreply.github.com>
2023-08-22 11:01:11 -0500

    Config harpoon plugin

--- init.lua
+++ init.lua
@@ -486,6 +486,23 @@ vim.defer_fn(function()
     },
   }
 end, 0)
+    swap = {
+      enable = false,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
+}
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
